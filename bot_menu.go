d07dc03877aac9c531065b01dd2fc7aa8b81d784
Serhii Mashentsev <d.ashesss@gmail.com>
2022-07-10 21:10:58 +0300

    implement available updates menu

--- bot_menu.go
+++ bot_menu.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/d-ashesss/news-feed-bot/pkg/model"
 	"gopkg.in/tucnak/telebot.v2"
 )
@@ -31,10 +32,17 @@ type BotMenuCategoriesUpdates struct {
 	Menu *telebot.ReplyMarkup
 }
 
-func NewBotMenuCategoriesUpdates(_ []model.Subscription) *BotMenuCategoriesUpdates {
+func NewBotMenuCategoriesUpdates(subs []model.Subscription) *BotMenuCategoriesUpdates {
 	m := &BotMenuCategoriesUpdates{
 		Menu: &telebot.ReplyMarkup{},
 	}
+	rows := make([]telebot.Row, 0, len(subs))
+	for _, sub := range subs {
+		label := fmt.Sprintf("%s (%d)", sub.Category.Name, sub.Unread)
+		btn := m.Menu.Data(label, "btnMenuCategoriesUpdates", sub.Category.ID)
+		rows = append(rows, m.Menu.Row(btn))
+	}
+	m.Menu.Inline(rows...)
 	return m
 }
 
