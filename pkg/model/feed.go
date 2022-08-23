2b7eabd29b15c10a9d301d58c24661f74075f2ac
Serhii Mashentsev <d.ashesss@gmail.com>
2022-08-23 12:12:25 +0300

    add feed model

---
+++ pkg/model/feed.go
@@ -0,0 +1,25 @@
+package model
+
+import (
+	"context"
+	"time"
+)
+
+type Feed struct {
+	ID         string    // ID is an internal ID.
+	Category   *Category // Category is the category of the feed.
+	Title      string    // Title is the title of the feed.
+	URL        string    // URL is a http link to the feed.
+	LastUpdate time.Time // LastUpdate is the published time of the last update fetched from the feed.
+}
+
+type FeedModel interface {
+	// Create saves a Feed entity into the DB.
+	Create(ctx context.Context, f *Feed) (string, error)
+	// Get retrieves a Feed entity from the DB.
+	Get(ctx context.Context, id string) (*Feed, error)
+	// GetAll retrieves all Feed entities for provided Category from the DB.
+	GetAll(ctx context.Context, cat *Category) ([]Feed, error)
+	// Delete deletes a Feed entity from the DB.
+	Delete(ctx context.Context, c *Feed) error
+}
