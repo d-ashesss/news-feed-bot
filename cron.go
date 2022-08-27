7531ab9150eaeaa019c3f691ebb52a5e2afcbbb3
Serhii Mashentsev <d.ashesss@gmail.com>
2022-08-27 13:48:02 +0300

    implement fetch updates cron

--- cron.go
+++ cron.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"context"
+	"github.com/d-ashesss/news-feed-bot/pkg/feed/fetcher"
+	"github.com/d-ashesss/news-feed-bot/pkg/model"
 	"log"
 	"net/http"
+	"time"
 )
 
 func (a *App) authCron(res http.ResponseWriter, r *http.Request) {
@@ -11,6 +15,33 @@ func (a *App) authCron(res http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func (a *App) handleCronFetch() {
-	log.Printf("fetch done")
+func (a *App) handleCronFetch(res http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	cats, err := a.CategoryModel.GetAll(ctx)
+	if err != nil {
+		log.Printf("get categories: %v", err)
+		res.WriteHeader(500)
+		return
+	}
+	for _, cat := range cats {
+		a.helperFetchCategory(ctx, &cat)
+	}
+}
+
+func (a *App) helperFetchCategory(ctx context.Context, cat *model.Category) {
+	feeds, err := a.FeedModel.GetAll(ctx, cat)
+	if err != nil {
+		log.Printf("get feeds in %q: %s", cat.Name, err)
+		return
+	}
+
+	f := fetcher.New(a.SubscriptionModel)
+	for _, feed := range feeds {
+		if err := f.Fetch(ctx, &feed, cat); err != nil {
+			log.Printf("fetch updates from %q: %v", feed.Title, err)
+		}
+		if err := a.FeedModel.SetUpdated(ctx, &feed, time.Now().UTC()); err != nil {
+			log.Printf("update feed %q: %v", feed.Title, err)
+		}
+	}
 }
